4312fa698e91f24bf3cca1015cb81a580a4b3326
MohamedHegazy00 <mohamd.hegazi@el-eng.menofia.edu.eg>
2026-03-03 22:46:44 +0200

    task

---
+++ python/Problem_Solving/day_2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        num_map = {}
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            
+            if complement in num_map:
+                return [num_map[complement], i]
+            
+            num_map[num] = i
+        
+        return []
+
+sol = Solution()
+print(sol.twoSum([2, 7, 11, 15], 9))
